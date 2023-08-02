1c9376372763693b76ca486420ffdc42e6ff1750
11jo <79876551+11jo@users.noreply.github.com>
2023-08-03 00:22:08 +0200

    Préfixes for Areas and other corrections

--- PoFQuestPack/D/XGBOY.d
+++ PoFQuestPack/D/XGBOY.d
@@ -1,11 +1,18 @@
 BEGIN ~XGBOY~
 
-IF ~NumTimesTalkedTo(0) See([PC])~ THEN BEGIN 0
+IF ~NumTimesTalkedTo(0)
+See(Player1)~ THEN BEGIN 0
   SAY @0
   IF ~~ THEN REPLY @1 GOTO 1
 END
 
 IF ~~ THEN BEGIN 1
   SAY @2
-  IF ~~ THEN DO ~EscapeArea()~ EXIT
+  IF ~~ THEN REPLY @3 GOTO 2
+END
+
+IF ~~ THEN BEGIN 2
+  SAY @4
+  IF ~~ THEN REPLY @5 DO ~AddJournalEntry(@50118,QUEST_DONE) EscapeArea()~ EXIT
+  IF ~~ THEN REPLY @7 DO ~AddJournalEntry(@50114,QUEST) RevealAreaOnMap("XG0033") EscapeArea()~ EXIT
 END
