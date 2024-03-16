9be3f8110e1de7cebf6b7f4233039688b1e2e144
11jo <79876551+11jo@users.noreply.github.com>
2024-03-16 13:28:13 +0100

    typo for Player6, thanks to Psychlopes

--- PoFQuestPack/D/XGFNAS.d
+++ PoFQuestPack/D/XGFNAS.d
@@ -12,7 +12,7 @@ ActionOverride(Player2,LeaveAreaLUA("XG0030","",[3120.2670],0))
 ActionOverride(Player3,LeaveAreaLUA("XG0030","",[3220.2640],0))
 ActionOverride(Player4,LeaveAreaLUA("XG0030","",[3020.2650],0))
 ActionOverride(Player5,LeaveAreaLUA("XG0030","",[3090.2600],0))
-ActionOverride(Player1,LeaveAreaLUA("XG0030","",[3260.2620],0))
+ActionOverride(Player6,LeaveAreaLUA("XG0030","",[3260.2620],0))
 EscapeArea()
 ~ EXIT
   IF ~~ THEN REPLY @5 EXIT
@@ -25,7 +25,7 @@ ActionOverride(Player2,LeaveAreaLUA("XG0030","",[3120.2670],0))
 ActionOverride(Player3,LeaveAreaLUA("XG0030","",[3220.2640],0))
 ActionOverride(Player4,LeaveAreaLUA("XG0030","",[3020.2650],0))
 ActionOverride(Player5,LeaveAreaLUA("XG0030","",[3090.2600],0))
-ActionOverride(Player1,LeaveAreaLUA("XG0030","",[3260.2620],0))
+ActionOverride(Player6,LeaveAreaLUA("XG0030","",[3260.2620],0))
 ~ EXIT
   IF ~~ THEN REPLY @8 EXIT
 END
