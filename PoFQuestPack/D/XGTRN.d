41797e6fc1b7f345025718eb43e5eed9cf0d56c2
11jo <79876551+11jo@users.noreply.github.com>
2024-03-21 00:46:14 +0100

    More correction thanks to Psychlopes

--- PoFQuestPack/D/XGTRN.d
+++ PoFQuestPack/D/XGTRN.d
@@ -1,9 +1,9 @@
 BEGIN ~XGTRN~
 
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN 0
+IF ~True()~ THEN BEGIN 0
   SAY @1
   IF ~~ THEN REPLY @2 DO ~StartStore("XGSHOP3",LastTalkedToBy(Myself))
 ~ EXIT
-  IF ~~ THEN REPLY @3 DO ~SetNumTimesTalkedTo(0)
-~ EXIT
+  IF ~~ THEN REPLY @3 EXIT
 END
+
